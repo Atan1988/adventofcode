4134735c55ce11f95f274c53b01bac0f12edf52d
Atan1988 <tzw0423@hotmail.com>
2019-12-13 08:36:54 -0500

    partial 11

---
+++ R/problem11.R
@@ -0,0 +1,23 @@
+library(dplyr)
+source('R/intcomputer.R')
+options("scipen"=100)
+
+direc <- pi/2
+change_direc <- function(direc, direc_input) {
+  #0 for left, 1 for right
+  if (direc_input == 0) return(direc + pi/2)
+  if (direc_input == 1) return(direc - pi/2)
+  return(NULL)
+}
+
+new_coord <- function(curr_coord, direc) {
+  x_incre <- round(cos(direc), 10)
+  y_incre <- round(sin(direc), 10)
+  return(c(curr_coord[1] + x_incre, curr_coord[2] + y_incre))
+}
+
+###robot state is a list
+##with elements for curr_coord, curr_direc, data_df
+robot_program <- function(robot_state){
+  
+}

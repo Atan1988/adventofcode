79180b1d74ed0b3ad097b0d4b2e352cd29fa01c6
Atan1988 <tzw0423@hotmail.com>
2019-12-26 00:10:06 -0500

    commit

--- R/problem18.R
+++ R/problem18.R
@@ -40,7 +40,7 @@ tree_structure[[as.character(which(input_map1 == 'a'))]]
 
 closest_intsect <- function(y, dir, cap = 1000) {
   i <- 0; orig <- y;
-  door <- NULL
+  door <- NULL; key <- NULL
   while (length(y$child_loc) < 3 | y$loc == orig$loc ){
     if (dir %in% names(y$child_loc)) tmp_dir <- dir
     if (!dir %in% names(y$child_loc)) {
@@ -49,13 +49,14 @@ closest_intsect <- function(y, dir, cap = 1000) {
     }
     dir <- tmp_dir; tmp_y <- tree_structure[[as.character(y$child_loc[tmp_dir])]]
     if (tmp_y$val %in% LETTERS) door <- c(door, tmp_y$val)
+    if (tmp_y$val %in% letters) key <- c(key, tmp_y$val)
     y <- tmp_y 
     i <- i + 1; 
     if(i> cap) break
     if(length(y$child_loc) == 1) break
   }
   if (length(y$child_loc) == 1) return(NULL)
-  return(list(loc = y$loc, step = i, door = door))
+  return(list(loc = y$loc, step = i, door = door, key = key))
 }
 search_for_closest_intsects <- function(intsec) {
   x <- tree_structure[[intsec]]
@@ -75,26 +76,191 @@ intersect_map <- names(intersects_3or4) %>%
   })
 names(intersect_map ) <- names(intersects_3or4)
 
+calc_dist <- function(chain){
+  moves <- strsplit(chain, '_')[[1]]
+  start <- intersect_map[[moves[1]]]
+  step <- 0; door <- NULL; key <- NULL
+  for (i in 2:length(moves)){
+    tmp_int <- intersect_map[[moves[i-1]]]
+    idx <- which(purrr::map_chr(tmp_int[[1]], ~as.character(.$loc)) == moves[i])
+    step <- step + tmp_int[[1]][[idx]]$step
+    door <- c(door, tmp_int[[1]][[idx]]$door)
+    key <- c(key, tmp_int[[1]][[idx]]$key)
+  }
+  return(tibble(chain = chain, step = step, 
+                door = paste(sort(door), collapse = ","), 
+                key = paste(sort(key), collapse = ",")))
+}
 dist_btw_intsects <- function(intsec1, intsec2, depth = 30){
-  res <- intsec1
+  res <- intsec1; #tic(); depth = 30;
   for (i in 1:depth) {
-    res <- res %>% purrr::map(function(x){
-      tmp_x <- strsplit(x, "_")[[1]] 
+    res <- res %>% strsplit("_") %>% 
+      purrr::map(function(x){
+      tmp_x <- x; chain_x <- paste(x, collapse = "_")
       x1 <- tmp_x[length(tmp_x)]
-      if (x1 == intsec2) return(x)
+      if (x1 == intsec2) return(chain_x)
       loc <- purrr::map_chr(intersect_map[[x1]][[1]], ~.$loc)
       if (length(tmp_x)>=2) {
         x2 <- tmp_x[length(tmp_x)-1]
         loc <- loc[!loc %in% x2]
       }
-      if (length(loc)>0) res1 <- paste0(x, "_", loc) else res1 <- x
+      if (length(loc)>0) res1 <- paste0(chain_x,  "_", loc) else res1 <- chain_x
       return(res1)
     }) %>% unlist()
-  }
+    trim_idx <- res %>% strsplit('_') %>%
+        purrr::map_lgl(function(x) max(table(x))>1)
+    res[which(!trim_idx)] -> res
+    trim_idx2 <- res %>% strsplit('_') %>%
+      purrr::map_lgl(function(x) x[length(x)] != intsec2 & length(x) < i )
+    res[which(!trim_idx2)] -> res
+  }; #toc()
+  res1 <- res[which(res %>% strsplit('_') %>% purrr::map_lgl(~.[length(.)] == intsec2))]
+  dist_res1 <- purrr::map_df(res1, calc_dist) %>% 
+    group_by(door) %>% filter(step == min(step))
+  return(dist_res1)
 }
 
-loc0 <- as.character(which(input_map1 == '@'))
-loc1 <- as.character(which(input_map1 == 'G'))
+get_child_int <- function(intsec){
+  tibble::tibble(
+    loc  = purrr::map_chr(intsec[[1]], ~as.character(.$loc)), 
+    step = purrr::map_dbl(intsec[[1]], ~.$step), 
+    door = purrr::map_chr(intsec[[1]], ~paste(sort(.$door), collapse = ",")), 
+    key = purrr::map_chr(intsec[[1]], ~paste(sort(.$key), collapse = ","))
+  )
+}
 
-close_intsec0 <- search_for_closest_intsects(loc0)
-close_intsec1 <- search_for_closest_intsects(loc1)
+add_doors <- function(x, y, z){
+  door_list <- list(x, y, z)
+  purrr::pmap_chr(door_list, function(a, b, c){
+    tmp <- paste(unique(sort(strsplit(c(a, b, c), ',') %>% unlist()) %>% 
+                   .[. != ""]), collapse = ",")
+  })
+}
+
+find_dist_btw_points <- function(loc0, loc1, depth = 30) {
+  close_intsec0 <- search_for_closest_intsects(loc0)
+  close_intsec1 <- search_for_closest_intsects(loc1)
+  
+  intsec_df0 <- get_child_int(close_intsec0)
+  intsec_df1 <- get_child_int(close_intsec1)
+  purrr::cross_df(list(intsec0 = intsec_df0$loc, intsec1 = intsec_df1$loc)) %>% 
+    inner_join(intsec_df0, by = c('intsec0' = 'loc')) %>% 
+    inner_join(intsec_df1, by = c('intsec1' = 'loc')) %>% 
+    filter(intsec0 != intsec1) %>% 
+    purrrlyr::by_row(~dist_btw_intsects(.$intsec0, .$intsec1, depth = depth)) %>% 
+    tidyr::unnest(cols = '.out') %>% 
+    mutate(tot_step = step + step.x + step.y)  %>% 
+    mutate(tot_door = add_doors(door, door.x, door.y)) %>% 
+    mutate(tot_key = add_doors(key, key.x, key.y)) %>% 
+    group_by(tot_door) %>% 
+    filter(tot_step == min(tot_step)) -> test_df
+  return(test_df)
+}
+
+find_key <- function(key){
+  loc0 <- as.character(which(input_map1 == '@'))
+  loc1 <- as.character(which(input_map1 == key))
+  
+  find_dist_btw_points(loc0, loc1) %>% ungroup() %>% 
+    select(tot_step, tot_door, tot_key, chain) %>% distinct() %>% 
+    mutate(loc1 = !!key, 
+           chain = paste0(chain, '_', !!loc1)) 
+}
+
+trim_routes <- function(routes) {
+  keep <- routes %>% 
+    purrr::map(function(x){
+      tmp_r <- routes[grepl(x, routes)]
+      if (max(nchar(tmp_r))> nchar(x)) return(NULL)
+      return(x)
+    }) %>% unlist()
+  keep
+}
+
+check_route <- function(chain, keys0){
+  mvs <- chain %>% strsplit('_') %>% .[[1]]
+  tmp_key <- keys0;
+  key_collected <- NULL;
+  key_used <- NULL
+  for (i in 2:length(mvs) ){
+    if (i == length(mvs)){
+      tmp_int <- search_for_closest_intsects(mvs[i])
+      tmp_y <- tmp_int[[1]][[which(tmp_int[[1]] %>% purrr::map_lgl(~.$loc == mvs[i-1]))]]
+    } else {
+      tmp_int <- intersect_map[[mvs[i-1]]]
+      tmp_y <- tmp_int[[1]][[which(tmp_int[[1]] %>% purrr::map_lgl(~.$loc == mvs[i]))]]
+    }
+   
+    if (!is.null(tmp_y$door)) {
+      key_required <- tolower(tmp_y$door)
+      if(!key_required %in% tmp_key) return(tibble(open = F, 
+                    key_used = paste(key_used, collapse = ","), 
+                    key_collected = paste(key_collected, collapse = ",")))
+      key_used <- c(key_used, key_required)
+    }
+    if (!is.null(tmp_y$key)) {
+      tmp_key <- c(tmp_key, tmp_y$key)
+      key_collected <- c(key_collected, tmp_y$key)
+    }
+  }
+  return(tibble(open = T, key_used = paste(key_used, collapse = ","), 
+                key_collected = paste(key_collected, collapse = ",")))
+}
+
+clear_all_routes <- function(key_res){
+  keys <- list(); 
+  chains <- list();
+  rounds <- list()
+  i <- 1; l_chain <- 0
+  while (l_chain < length(key_res$chain)){
+    if (i == 1) {
+      tmp_keys <- NULL
+      tmp_chains <- NULL
+    } else {
+      tmp_keys <- keys[[i-1]]
+      tmp_chains <- chains[[i-1]]
+    }
+    round0 <- key_res %>% 
+      filter(!chain %in% tmp_chains) %>% 
+      purrrlyr::by_row(
+        function(row){
+          check_route(row$chain, keys0 = tmp_keys)
+        }
+      ) %>% tidyr::unnest(cols = '.out') %>% 
+      mutate(key_collected = ifelse(open, 
+              paste0(key_collected, ifelse(key_collected != "", ",", ""), loc1), 
+              key_collected), 
+             rounds = i) %>% 
+      filter(open) 
+    
+    round0_b <- round0 %>% 
+      filter(key_collected %in% trim_routes(key_collected))
+    
+    keys[[i]] <- c(tmp_keys, round0_b$key_collected) %>% strsplit(",") %>% 
+      unlist() %>% unique() %>% sort()
+    chains[[i]] <- c(tmp_chains, round0$chain)
+    rounds[[i]] <- round0_b
+    l_chain <- length(chains[[i]])
+    i <- i + 1
+  }
+  return(list(keys = keys, chains = chains, rounds = rounds))
+}
+
+print_map <- function(input, start, end){
+  input[start] <- '?'; input[end] <- "!"
+  writeLines(1:81 %>% purrr::map_chr(~paste(input[., ], collapse = "")), 'ppp.txt')
+}
+
+key_res <- pbapply::pblapply(letters, find_key, cl = 5) %>% bind_rows()
+
+key_res <- key_res %>% 
+  mutate(chain = gsub(paste0(3281 + c(1, -1, 81, -81), "_") %>% 
+                        paste(collapse = "|"), "", chain)) %>% 
+  distinct()
+
+key_res1 <- key_res %>% filter(purrr::map2_lgl(.x = loc1, 
+            .y = tot_key, function(x,y) !grepl(x, y)))
+
+clear_res <- clear_all_routes(key_res1)
+  
+rounds_df <- clear_res$rounds %>% bind_rows()
